442f8a25f822a43359b585bcfad4c9c25e830e31
Heena <heenashaikh1811@gmail.com>
2026-04-19 21:29:32 +0530

    reversenumber

---
+++ src/com/reversenumbers/example1.java
@@ -0,0 +1,32 @@
+package com.reversenumbers;
+
+public class example1 {
+
+	public static void main(String[] args) {
+		int number = 123;
+		int reverseNumber=0;
+		int lastDigit;
+		while(number!=0) {
+		lastDigit = number %10;
+		//Check for overflow
+		if(reverseNumber> Integer.MAX_VALUE/10 || (reverseNumber==Integer.MAX_VALUE/10 &&lastDigit>7));
+		{System.out.println(0);
+		System.exit(0);}
+		if(reverseNumber> Integer.MAX_VALUE/10 || (reverseNumber==Integer.MAX_VALUE/10 &&lastDigit>-8));
+		{System.out.println(0);
+		System.exit(0);} //underflow return 0
+		reverseNumber=reverseNumber*10+lastDigit;
+		number=number/10;
+		
+//		lastDigit = number %10;
+//		reverseNumber=reverseNumber*10+lastDigit;
+//		number=number/10;
+		
+//		lastDigit = number %10;
+//		reverseNumber=reverseNumber*10+lastDigit;
+//		number=number/10;
+		}
+		System.out.println(reverseNumber);
+	}
+
+}
