e46ea06d636037c8a35100defdd3a24b1089a75b
Heena <heenashaikh1811@gmail.com>
2026-04-20 13:37:03 +0530

    even or odd numbers

--- src/com/reversenumbers/example1.java
+++ src/com/reversenumbers/example1.java
@@ -3,25 +3,31 @@ package com.reversenumbers;
 public class example1 {
 
 	public static void main(String[] args) {
-		int number = 123; //123456789    
-		int reverseNumber=0;
+		int number = 123; // 123456789
+		int reverseNumber = 0;
 		int lastDigit;
-		while(number!=0) {
-		lastDigit = number %10;
-		//Check for overflow
-		if(reverseNumber> Integer.MAX_VALUE/10 || (reverseNumber==Integer.MAX_VALUE/10 &&lastDigit>7));
-		{System.out.println(0);
-		System.exit(0);}
-		if(reverseNumber> Integer.MAX_VALUE/10 || (reverseNumber==Integer.MAX_VALUE/10 &&lastDigit>-8));
-		{System.out.println(0);
-		System.exit(0);} //underflow return 0
-		reverseNumber=reverseNumber*10+lastDigit;
-		number=number/10;
-		
+		while (number != 0) {
+			lastDigit = number % 10;
+			// Check for overflow
+			if (reverseNumber > Integer.MAX_VALUE / 10 || (reverseNumber == Integer.MAX_VALUE / 10 && lastDigit > 7))
+				;
+			{
+				System.out.println(0);
+				System.exit(0);
+			}
+			if (reverseNumber > Integer.MAX_VALUE / 10 || (reverseNumber == Integer.MAX_VALUE / 10 && lastDigit > -8))
+				;
+			{
+				System.out.println(0);
+				System.exit(0);
+			} // underflow return 0
+			reverseNumber = reverseNumber * 10 + lastDigit;
+			number = number / 10;
+
 //		lastDigit = number %10;
 //		reverseNumber=reverseNumber*10+lastDigit;
 //		number=number/10;
-		
+
 //		lastDigit = number %10;
 //		reverseNumber=reverseNumber*10+lastDigit;
 //		number=number/10;
---
+++ src/comevenoroddnumbers/example1.java
@@ -0,0 +1,14 @@
+package comevenoroddnumbers;
+
+public class example1 {
+	public static void main(String[] args) {
+		int number =24;
+		if((number & 1)==1) {
+			System.out.println("ODD");
+		}
+		else
+		{
+			System.out.println("EVEN");
+		}
+	}
+}
